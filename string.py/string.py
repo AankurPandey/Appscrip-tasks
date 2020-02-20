1cb985a1e23ac6fc142595713c2398775805a242
ankurNavgurukul <61147254+ankurNavgurukul@users.noreply.github.com>
2020-02-20 15:14:06 +0530

    Update string.py

--- string.py/string.py
+++ string.py/string.py
@@ -2,20 +2,20 @@
 
 #function Check_digits return true if string contains only digits else return false
 # def check_digits(string):
-# 	digitsNumber=0
+# 	digitsNumber=0 #variable to count the number of character in string
 # 	digits='0123456789'
-# 	for character in string:
+# 	for character in string:#loop to read character of string one by one 
 # 		if character in digits:
 # 			digitsNumber+=1
 
-# 	if digitsNumber==len(string):
+# 	if digitsNumber==len(string):#comparing value of digitsNumber with length of string
 # 		return True
 # 	else:
 # 		return False
 
 
-# string=input('Enter the string to check\t\t')
-# answer=check_digits(string)
+# string=input('Enter the string to check\t\t')  #taking a string as an input
+# answer=check_digits(string) #function calling statement for check_digits
 
 # if answer=='True':
 # 	print('Your entered string contains only digits')
@@ -24,7 +24,9 @@
 
 
 ###############################################
-#program to check if a given string is a palindrome.		
+#program to check if a given string is a palindrome.	
+
+#function check_palindrome check whether the string is palindrome or not
 # def check_palindrome(string):
 # 	repeate=len(string)//2
 # 	for characterIndex in range(repeate):
@@ -35,8 +37,8 @@
 # 	else:
 # 		return False
 
-# string=input('Enter the string to check\t\t')		
-# answer=check_palindrome(string)
+# string=input('Enter the string to check\t\t')		#taking a string as an input
+# answer=check_palindrome(string) #function calling statement for checking palindrome
 
 # if answer==True:
 # 	print('Your entered string is a palindrome')
