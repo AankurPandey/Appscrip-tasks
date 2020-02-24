75e1b4932af63b8ca144f14da1382a3abd309f8c
Ankur Pandey <61147254+ankurNavgurukul@users.noreply.github.com>
2020-02-24 17:43:26 +0530

    Update list.py

--- list.py/list.py
+++ list.py/list.py
@@ -1,25 +1,16 @@
-#######################################task 1
-#function to return minimum value
-def find_minimum(List):
-	minimum=List[0] # variable storing first value of list
-	for value in List:# loop for getting vlues of list one by one
-		if value<minimum:
-			minimum=value
-	return minimum
+				#task1
+# program to print the missing numbers from 1 to 100 in the list
 
-#function to return maximum value	
-def find_maximum(List):
-	maximum=List[0] #variable storing first value of list
-	for value in List:
-		if value>maximum:
-		 	maximum=value
-	return maximum
-		 
+# function printing the missing numbers from 1 to 100 in passed list
+def find_missing(List):
+	for number in range(1,101):
+		if number not in List:
+			print(number,' not present')
+
 		 '''here we r taking input from user for a list'''
 List=list(map(int,input('Enter the values of list on a single line with spaces\t\t ').strip().split())) 
+find_missing(List)#function calling statement	
 
-print('Minimum value of list is ',find_minimum(List))#calling statement for finding minimum term
-print('Maximum value of list is ',find_maximum(List))#calling statement for finding maximum term
 
 ######################################task 2
 #program to find the dublicates in list
@@ -41,18 +32,33 @@ List=list(map(int,input('Enter the values of list on a single line with spaces\t
 
 find_dublicates(List)
 ####################################################################
-# program to print the missing numbers from 1 to 100 in the list
+						#task 3
+#program to find the largest and smallest number in an unsorted integer array
 
-# function printing the missing numbers from 1 to 100 in passed list
-def find_missing(List):
-	for number in range(1,101):
-		if number not in List:
-			print(number,' not present')
+#function to return minimum value
+def find_minimum(array):
+	minimum=array[0] # variable storing first value of list
+	for value in array:# loop for getting vlues of list one by one
+		if value<minimum:
+			minimum=value
+	return minimum
 
+#function to return maximum value	
+def find_maximum(array):
+	maximum=array[0] #variable storing first value of list
+	for value in array:
+		if value>maximum:
+		 	maximum=value
+	return maximum
+		 
 		 '''here we r taking input from user for a list'''
-List=list(map(int,input('Enter the values of list on a single line with spaces\t\t ').strip().split())) 
-find_missing(List)#function calling statement
-################################################################	
+array=list(map(int,input('Enter the values of list on a single line with spaces\t\t ').strip().split())) 
+
+print('Minimum value of list is ',find_minimum(array))#calling statement for finding minimum term
+print('Maximum value of list is ',find_maximum(array))#calling statement for finding maximum term
+################################################################
+						#task4
+
 #program to find all pairs of an integer array whose sum is equal to a given number.
 
 #function printing the pairs
